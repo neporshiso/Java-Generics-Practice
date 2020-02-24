68f8ff0912372cd1a196b4f1eb234b4553c9dac4
Nep T. Orshiso <nep.orshiso@gmail.com>
2020-02-23 21:17:13 -0500

    removed comment prompt

--- src/client/AggregatorApp.java
+++ src/client/AggregatorApp.java
@@ -1,31 +1,18 @@
 package client;
 
 import aggregators.AggregatorProcessor;
-import aggregators.MinAggregator;
+import aggregators.MaxAggregator;
 
 import java.io.IOException;
 
 public class AggregatorApp {
 
 	public static void main(String[] args) throws IOException {
-		
-		/**
-			You'll need to uncomment the below code. You'll notice it does not compile!
-			
-			There are 2 objectives in this assignment.
-			
-			1).  Make sure the code compiles correctly after you uncomment it below.
-			2).  Make sure it runs as explained in the assignment video!
-			
-			-->> YOUR WORK SHOULD BE DONE IN THE AggregatorProcessor CLASS. 
-		**/
 
-		MinAggregator agg = new MinAggregator();
-		AggregatorProcessor<MinAggregator> aggsProcessor = new AggregatorProcessor<>(agg, "table.csv");
+		MaxAggregator agg = new MaxAggregator();
+		AggregatorProcessor<MaxAggregator> aggsProcessor = new AggregatorProcessor<>(agg, "table.csv");
 		double value = aggsProcessor.runAggregator(1);
 		System.out.println(value);
-	
-
 	}
 
 }
