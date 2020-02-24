57e7ceaadfecb2fdb6efa8032d669d731cbf4fd6
Nep T. Orshiso <nep.orshiso@gmail.com>
2020-02-23 21:45:30 -0500

    added some notes after reviewing official solution

--- src/aggregators/AggregatorProcessor.java
+++ src/aggregators/AggregatorProcessor.java
@@ -7,13 +7,16 @@ import java.util.List;
 
 public class AggregatorProcessor <M extends Aggregator> {
 
+    // I didn't have to declare numbers here as I have access to numbers in the parent class Aggregator
     List<Double> numbers;
     Aggregator agg;
     String s;
 
+    // could have named s better... it's a filepath
     public AggregatorProcessor(M agg, String s) {
         this.agg = agg;
         this.s = s;
+        // unnecessary since agg already has numbers
         this.numbers = agg.getValues();
     }
 
@@ -25,6 +28,7 @@ public class AggregatorProcessor <M extends Aggregator> {
              ) {
             String[] split = num.split(",");
             Double val = Double.parseDouble(split[i-1]);
+            // in the officialy solution, agg.add(val) works because there is already an add method implemented in Aggregator that appends a Double to a List of Doubles
             numbers.add(val);
         }
 
